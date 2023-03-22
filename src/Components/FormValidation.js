25d7ec3140c5b98c2843861bd0f492e939752d1b
harisishtiaq1 <harisharry232@gmail.com>
2023-03-22 11:15:47 +0500

    Updated

--- src/Components/FormValidation.js
+++ src/Components/FormValidation.js
@@ -1,77 +1,61 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
-
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 function FormValidation() {
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-  const [confirmPassword, setConfirmPassowrd] = useState();
-  const [errors,setErrors]=useState(false)
 
-  const onSubmit = (event) => {
-    event.preventDefault();
+  const onSubmit = () => {
     console.log("name", name);
     console.log("email", email);
     console.log("password", password);
-    console.log("confirmPassword", confirmPassword);
   };
   return (
-      <Box
-      component='form'
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          flexDirection: "column",
-          mt: 5,
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <Typography component="h1" variant="h5">
-          Form Validation
-        </Typography>
-        <TextField
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          label="Name"
-          sx={{ mt: 2, width: 300 }}
-          color="secondary"
-        ></TextField>
-        
-        <TextField
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          label="Email"
-          sx={{ mt: 2, width: 300 }}
-          color="secondary"
-        ></TextField>
+    <Box
+      component="form"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        mt: 5,
+      }}
+      noValidate
+      autoComplete="off"
+      onClick={onSubmit}
+    >
+      <Typography component="h1" variant="h5">
+        Form Validation
+      </Typography>
+      <TextField
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        label="Name"
+        sx={{ mt: 2, width: 300 }}
+        color="secondary"
+      ></TextField>
 
-        <TextField
-        type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          label="Password"
-          sx={{ mt: 2, width: 300 }}
-          id="Password"
-          color="secondary"
-        ></TextField>
-        <TextField
-        type='password'
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassowrd(e.target.value)}
-          label="Confirm Password"
-          sx={{ mt: 2, width: 300 }}
-          color="secondary"
-        ></TextField>
-        <Button
-          variant="contained"
-          sx={{ mt: 3, backgroundColor: "blueviolet" }}
-          onClick={onSubmit}
-        >
-          Submit
-        </Button>
-      </Box>
+      <TextField
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        label="Email"
+        sx={{ mt: 2, width: 300 }}
+        color="secondary"
+      ></TextField>
 
+      <TextField
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        label="Password"
+        sx={{ mt: 2, width: 300 }}
+        id="Password"
+        color="secondary"
+      ></TextField>
+      <Button variant="contained" sx={{ mt: 3, backgroundColor: "blueviolet" }}>
+        Submit
+      </Button>
+    </Box>
   );
 }
 
